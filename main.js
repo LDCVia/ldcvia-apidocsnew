f0a7f8418af97e88afd5b450be3d53becc1580ba
Matt White <Matt White>
2016-08-08 16:05:50 +0100

    Latest version of doco

--- main.js
+++ main.js
@@ -743,4 +743,24 @@ require([
         return results;
     }
 
+    var offset = 220;
+    var duration = 500;
+    var button = $('<a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>');
+    button.appendTo("body");
+
+    jQuery(window).scroll(function() {
+      if (jQuery(this).scrollTop() > offset) {
+          jQuery('.back-to-top').fadeIn(duration);
+      } else {
+          jQuery('.back-to-top').fadeOut(duration);
+      }
+    });
+
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    });
+
+
 });
